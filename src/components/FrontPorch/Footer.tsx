79971ba6df881d7d980658796c98d9cc17f2a0ac
CallenCaracy <diangcocalen@gmail.com>
2026-01-28 12:52:03 +0800

    Refresh git index to fix casing issues

--- src/components/FrontPorch/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="py-2 bg-black text-center text-white">
-      © {new Date().getFullYear()} PosterBoi. All rights reserved.
-    </footer>
-  );
-}
